d12259ad0b85e8032fee5d0b564e543c0b2b142b
Kawã karlyndo <160342064+Kawakarlyndo@users.noreply.github.com>
2025-03-25 22:39:55 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,60 +1,60 @@
 let dados = [
     {   
-        imagem: "./img/idiota.jpg",
+        imagem: "./idiota.jpg",
         nome: "O Idiota",
         descricao: "Romance de Fiódor Dostoiévski que explora a inocência e a bondade em um mundo corrompido.",
         link: "https://www.apple.com/macbook"
     },
     {
-        imagem: "./img/met.jpg",
+        imagem: "./met.jpg",
         nome: "A Metamorfose",
         descricao: "Novela de Franz Kafka sobre um homem que se transforma em um inseto gigante.",
         link: "https://www.apple.com/iphone"
     },
     {
-        imagem: "./img/noites.jpg",
+        imagem: "./noites.jpg",
         nome: "Noites Brancas",
         descricao: "Novela curta de Dostoiévski que retrata um romance efêmero nas noites de São Petersburgo.",
         link: "https://www.apple.com/macbook-air"
     },
     {
-        imagem: "./img/dec.jpg",
+        imagem: "./dec.jpg",
         nome: "O Declínio do Homem",
         descricao: "Obra que discute a evolução humana e os fatores que influenciam seu desenvolvimento.",
         link: "https://www.apple.com/macbook-air"
     },
     {
-        imagem: "./img/dom.jpg",
+        imagem: "./dom.jpg",
         nome: "Dom Casmurro",
         descricao: "Clássico de Machado de Assis que aborda o ciúme e a dúvida no relacionamento de Bentinho e Capitu.",
         link: "https://www.apple.com/macbook-air"
     },
     {
-        imagem: "./img/irma.jpg",
+        imagem: "./irma.jpg",
         nome: "Irmão Kasparov",
         descricao: "Ficção hipotética sobre um personagem inspirado no lendário enxadrista Garry Kasparov.",
         link: "https://www.apple.com/macbook-air"
     },
     {
-        imagem: "./img/ut.jpg",
+        imagem: "./ut.jpg",
         nome: "Utopia",
         descricao: "Livro de Thomas More que descreve uma sociedade ideal e suas estruturas políticas e sociais.",
         link: "https://www.apple.com/macbook-air"
     },
     {
-        imagem: "./img/anne.jpg",
+        imagem: "./anne.jpg",
         nome: "O Diário de Anne Frank",
         descricao: "Relato real de uma adolescente judia que se escondeu dos nazistas durante a Segunda Guerra Mundial.",
         link: "https://www.apple.com/macbook-air"
     },
     {
-        imagem: "./img/download.jpg",
+        imagem: "./download.jpg",
         nome: "Vinte Mil Léguas Submarinas",
         descricao: "Romance de Júlio Verne que narra as aventuras do Capitão Nemo a bordo do submarino Nautilus.",
         link: "https://www.apple.com/macbook-air"
     },
     {
-        imagem: "./img/piqueno.jpg",
+        imagem: "./piqueno.jpg",
         nome: "O Pequeno Príncipe",
         descricao: "Livro de Antoine de Saint-Exupéry que aborda temas como amor, amizade e sentido da vida.",
         link: "https://www.apple.com/macbook-air"
